76428dec625a5c5b625c781497991157eafa1fdd
cyril grinko <k.a.grinko@gmail.com>
2024-11-01 15:25:42 +0300

    refactored on request

--- matrix/matrix.hpp
+++ matrix/matrix.hpp
@@ -4,6 +4,7 @@
 template <std::size_t N, std::size_t M, typename T>
 class Matrix;
 
+namespace matrix_utils {
 template <std::size_t N, typename T>
 T GetTrace(const Matrix<N, N, T>& matrix) {
   T result = T();
@@ -12,50 +13,46 @@ T GetTrace(const Matrix<N, N, T>& matrix) {
   }
   return result;
 }
+}  // namespace matrix_utils
 
 template <std::size_t N, std::size_t M, typename T = int64_t>
 class Matrix {
+  using Buffer = std::array<std::array<T, M>, N>;
+
  public:
   Matrix() {
-    for (std::size_t i = 0; i < N; ++i) {
-      for (std::size_t j = 0; j < M; ++j) {
-        buffer_[i][j] = T();
-      }
-    }
+    DoForEveryElement([](std::size_t i, std::size_t j, Buffer& buffer) {
+      buffer[i][j] = T();
+    });
   }
 
   Matrix(const std::vector<std::vector<T>>& data) {
-    for (std::size_t i = 0; i < N; ++i) {
-      for (std::size_t j = 0; j < M; ++j) {
-        buffer_[i][j] = data[i][j];
-      }
-    }
+    DoForEveryElement(
+        [](std::size_t i, std::size_t j, Buffer& buffer,
+           const std::vector<std::vector<T>>& data) {
+          buffer[i][j] = data[i][j];
+        },
+        data);
   }
   Matrix(T elem) {
-    for (std::size_t i = 0; i < N; ++i) {
-      for (std::size_t j = 0; j < M; ++j) {
-        buffer_[i][j] = elem;
-      }
-    }
+    DoForEveryElement([](std::size_t i, std::size_t j, Buffer& buffer,
+                         T elem) { buffer[i][j] = elem; },
+                      elem);
   }
 
   ~Matrix() = default;
 
   Matrix& operator+=(const Matrix& other) {
-    for (std::size_t i = 0; i < N; ++i) {
-      for (std::size_t j = 0; j < M; ++j) {
-        buffer_[i][j] += other(i, j);
-      }
-    }
+    DoForEveryElement([](std::size_t i, std::size_t j, Buffer& buffer,
+                         const Matrix& other) { buffer[i][j] += other(i, j); },
+                      other);
     return *this;
   }
 
   Matrix& operator-=(const Matrix& other) {
-    for (std::size_t i = 0; i < N; ++i) {
-      for (std::size_t j = 0; j < M; ++j) {
-        buffer_[i][j] -= other(i, j);
-      }
-    }
+    DoForEveryElement([](std::size_t i, std::size_t j, Buffer& buffer,
+                         const Matrix& other) { buffer[i][j] -= other(i, j); },
+                      other);
     return *this;
   }
 
@@ -72,11 +69,9 @@ class Matrix {
   }
 
   Matrix& operator*=(const T& other) {
-    for (std::size_t i = 0; i < N; ++i) {
-      for (std::size_t j = 0; j < M; ++j) {
-        buffer_[i][j] *= other;
-      }
-    }
+    DoForEveryElement([](std::size_t i, std::size_t j, Buffer& buffer,
+                         const T& other) { buffer[i][j] *= other; },
+                      other);
     return *this;
   }
 
@@ -110,7 +105,7 @@ class Matrix {
     return result;
   }
 
-  T Trace() const { return GetTrace(*this); }
+  T Trace() const { return matrix_utils::GetTrace(*this); }
 
   T& operator()(std::size_t row, std::size_t column) {
     return buffer_[row][column];
@@ -124,5 +119,14 @@ class Matrix {
   }
 
  private:
-  std::array<std::array<T, M>, N> buffer_;
+  template <typename Func, typename... Args>
+  void DoForEveryElement(Func func, Args... args) {
+    for (std::size_t i = 0; i < N; ++i) {
+      for (std::size_t j = 0; j < M; ++j) {
+        func(i, j, buffer_, args...);
+      }
+    }
+  }
+
+  Buffer buffer_;
 };
